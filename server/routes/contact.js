44775e6729f1c91585cbc049ccadefc6339c5ec1
Johnblesson <johnblessonrowe@gmail.com>
2024-05-28 20:32:12 +0000

    update

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 const router = Router();
 
 import { createContacts, getContactForm, getAllContacts, messageView, deleteMessage } from "../controllers/contact.js";
+import ensureAuthenticated from "../middlewares/auth.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
 
 // Add more routes here
-router.get('/contact', getContactForm);
-router.post('/contact', createContacts);
-router.get('/all-messages', getAllContacts);
-router.get('/view-message/:id',  messageView)
-router.delete('/delete-message/:id', deleteMessage);
-router.get('/delete-message/:id', deleteMessage);
+router.get('/contact', ensureAuthenticated, getContactForm);
+router.post('/contact', ensureAuthenticated, createContacts);
+router.get('/all-messages', ensureAuthenticated, isAdmin, getAllContacts);
+router.get('/view-message/:id', ensureAuthenticated, isAdmin,  messageView)
+router.delete('/delete-message/:id', ensureAuthenticated, isAdmin, deleteMessage);
+router.get('/delete-message/:id', ensureAuthenticated, isAdmin, deleteMessage);
 export default router;
