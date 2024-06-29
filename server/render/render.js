311724d4a9372f602561d9c244dda683c416cfca
Johnblesson <johnblessonrowe@gmail.com>
2024-06-29 14:01:45 +0000

    update

--- server/render/render.js
+++ server/render/render.js
@@ -683,3 +683,49 @@ try {
   res.status(500).send("An error occurred while fetching the apartment details.");
 }
 };
+
+
+export const faq = async (req, res) => {
+  const getTimeOfDay = () => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour >= 5 && currentHour < 12) {
+      return 'Good Morning';
+    } else if (currentHour >= 12 && currentHour < 18) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Evening';
+    }
+  };
+
+  try {
+    // const apts = await Apartments.findOne({ _id: req.params.id });
+    //  // Find all verified apartments and sort them by sponsored status and createdAt timestamp in descending order
+    //  const apartments = await Apartments.find({ verification: 'verified' }).sort({ sponsored: -1, createdAt: -1 });
+
+    const greeting = getTimeOfDay();
+    const user = req.isAuthenticated() ? req.user : null;
+    const role = user ? user.role : null; // Get user role if user is authenticated
+
+    //  // Process each apartment to set photoUrl, formattedCreatedAt, and daysAgo
+    // apartments.forEach(apartment => {
+    //   // Ensure photoUrl is set properly
+    //   apartment.photoUrl = apartment.photo || ''; // Use empty string if no photo is available
+
+    //   // Format the createdAt date and calculate days ago
+    //   apartment.formattedCreatedAt = moment(apartment.createdAt).format('DD-MM-YYYY HH:mm');
+    //   apartment.daysAgo = moment().diff(moment(apartment.createdAt), 'days');
+    // });
+
+    res.render("faq", {
+      // apartments,
+      greeting,
+      user,
+      // apts,
+      role,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching apartments.");
+  }
+};
--- server/routes/viewRoutes.js
+++ server/routes/viewRoutes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 const router = Router();
 
-import { about, service, features, blog, allProperties, getPostApartment, getPostApartmentAdmin, listedProperties, allusers, guestPage } from "../render/render.js";
+import { about, service, features, blog, allProperties, getPostApartment, getPostApartmentAdmin, listedProperties, allusers, guestPage, faq } from "../render/render.js";
 import { adminAbout, adminFeatures, adminService, adminBlog, termsConditions, registrationProcessStatement } from "../render/admin.js";
 import { getAllUsers } from "../controllers/auth.js"
 import ensureAuthenticated from "../middlewares/auth.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
 import { isAccountant } from "../middlewares/isAccountant.js";
 import cacheMiddleware from "../middlewares/cacheMiddleware.js"
+import { checkSudoMiddleware } from '../middlewares/sudo.js';
+
 
 // Add more routes here
 // router.get("/all-properties", ensureAuthenticated, allProperties );
@@ -63,6 +65,8 @@ router.get("/admin-blog", cacheMiddleware, ensureAuthenticated, adminBlog);
 router.get("/terms-and-conditions", termsConditions)
 
 router.get("/registration-process-statement", registrationProcessStatement)
+
+router.get("/faq", cacheMiddleware, ensureAuthenticated, isAdmin, checkSudoMiddleware, faq)
 
 router.get("/signup", cacheMiddleware, (req, res) => {
     res.render("signup")    
